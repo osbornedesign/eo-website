01d04ca6f03d5e3d41fb3558c9fe43604e5cb948
Eric Osborne <ericosborne@Erics-MacBook-Pro.local>
2018-04-04 13:20:48 -0400

    progress layout and wireframe iamge

--- progress.php
+++ progress.php
@@ -81,25 +81,18 @@
 
       <div class="body-container">
         <div class="m-16-x-x-x row">
-          <div class="col-xs-12 col-md-6">
+          <div class="col-xs-12">
             <div class="p-x-16 m-16-x">
-              <img src="img/tl-homepage-wf.png">
+              <img src="img/progress-wf-1.png">
             </div>
           </div>
-
-          <div class="col-xs-12 col-md-6">
-            <div class="p-x-16 m-16-x">
-              <img src="img/USMLE-Product_Page.png">
-            </div>
-          </div>
-        </div>
       </div>
 
       <div class="body-container">
         <div class="m-64-x-x-x row">
           <div class="col-sm-12">
             <div class="m-16-x">
-              <h2>Illustrations &amp; Animations</h2>
+              <h3>Illustrations &amp; Animations</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo sem ac ipsum sagittis, et venenatis magna aliquam. Fusce in tortor diam. Mauris fermentum augue nec facilisis aliquam. Suspendisse lobortis feugiat rhoncus. Aliquam felis ipsum, consequat vulputate diam sit amet, posuere varius nibh. Mauris ex metus, rhoncus ac ex non, luctus sagittis arcu. Nunc faucibus neque ac pellentesque vestibulum. Quisque sed tristique metus. Nam eleifend suscipit varius. Donec non tempus libero, et laoreet urna. Proin sed mattis ligula.</p>
             </div>
           </div>
