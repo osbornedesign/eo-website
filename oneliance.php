fa13d0e36a62f382438101f00ac489083d51535f
Eric Osborne <ericosborne@Erics-MacBook-Pro.local>
2018-04-05 14:42:31 -0400

    thumbnail images

--- oneliance.php
+++ oneliance.php
@@ -2,95 +2,57 @@
 
   <main>
   	<div class="m-32-x-x-x">
-      <div class="m-x-x-64-x row row-full">
+      <div class="m-x-x-64-x row">
         <div class="col-sm-12">
-          <img style="max-width: 100%;" src="img/oneliance-header.png">
+          <img src="img/oneliance-header.png">
         </div>
       </div>
 
-      <div class="m-64-x-x-x row">
-        <div class="col-sm-12">
-          <div class="m-16-x">
-            <h2>Final Design</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo sem ac ipsum sagittis, et venenatis magna aliquam. Fusce in tortor diam. Mauris fermentum augue nec facilisis aliquam. Suspendisse lobortis feugiat rhoncus. Aliquam felis ipsum, consequat vulputate diam sit amet, posuere varius nibh. Mauris ex metus, rhoncus ac ex non, luctus sagittis arcu. Nunc faucibus neque ac pellentesque vestibulum. Quisque sed tristique metus. Nam eleifend suscipit varius. Donec non tempus libero, et laoreet urna. Proin sed mattis ligula.</p>
-          </div>
-        </div>
-      </div>
-
-  	  <div class="m-64-x-x-x row">
-  	    <div class="col-xs-12">
-  	      <div class="p-x-16 m-16-x">
-            <img style="max-width: 100%;" src="img/oneliance.jpg">
-  	      </div>
-  	    </div>
-  	  </div>
-
-      <div class="m-64-x-x-x row">
-        <div class="col-sm-12">
-          <div class="m-16-x">
-            <h2>Wireframes</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo sem ac ipsum sagittis, et venenatis magna aliquam. Fusce in tortor diam. Mauris fermentum augue nec facilisis aliquam. Suspendisse lobortis feugiat rhoncus. Aliquam felis ipsum, consequat vulputate diam sit amet, posuere varius nibh. Mauris ex metus, rhoncus ac ex non, luctus sagittis arcu. Nunc faucibus neque ac pellentesque vestibulum. Quisque sed tristique metus. Nam eleifend suscipit varius. Donec non tempus libero, et laoreet urna. Proin sed mattis ligula.</p>
+      <div class="body-container">
+        <div class="m-64-x-x-x row">
+          <div class="col-md-12">
+            <div class="p-x-16-x-x m-16-x">
+              <h2>Oneliance Website</h2>
+            </div>
           </div>
         </div>
       </div>
 
-      <div class="m-64-x-x-x row">
-        <div class="col-xs-12">
-          <div class="p-x-16 m-16-x">
-            <img style="max-width: 100%;" src="img/df-16-lo.png">
+      <div class="body-container">
+        <div class="row">
+          <div class="col-md-12 col-lg-8">
+            <div class="p-0-32-0-0">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo sem ac ipsum sagittis, et venenatis magna aliquam. Fusce in tortor diam. Mauris fermentum augue nec facilisis aliquam. Suspendisse lobortis feugiat rhoncus. Aliquam felis ipsum, consequat vulputate diam sit amet, posuere varius nibh. Mauris ex metus, rhoncus ac ex non, luctus sagittis arcu. Nunc faucibus neque ac pellentesque vestibulum. Quisque sed tristique metus. Nam eleifend suscipit varius. Donec non tempus libero, et laoreet urna. Proin sed mattis ligula.</p>
+            </div>
+          </div>
+          <div class="col-md-12 col-lg-4">
+            <div class="m-16-x p-32 role-content">
+              <p class="m-x-x-16-x"><strong>Role:</strong> Vision and strategy, creative direction, UX and visual design, team management</p>
+              <p class="m-x-x-16-x"><strong>Tools used:</strong> Adobe XD, Illustrator, Invision, After Effects</p>
+            </div>
           </div>
         </div>
       </div>
 
-
-      <div class="m-64-x-x-x row">
-        <div class="col-sm-12">
-          <div class="m-16-x">
-            <h2>Illustrations &amp; Animations</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo sem ac ipsum sagittis, et venenatis magna aliquam. Fusce in tortor diam. Mauris fermentum augue nec facilisis aliquam. Suspendisse lobortis feugiat rhoncus. Aliquam felis ipsum, consequat vulputate diam sit amet, posuere varius nibh. Mauris ex metus, rhoncus ac ex non, luctus sagittis arcu. Nunc faucibus neque ac pellentesque vestibulum. Quisque sed tristique metus. Nam eleifend suscipit varius. Donec non tempus libero, et laoreet urna. Proin sed mattis ligula.</p>
+      <div class="body-container">
+        <div class="m-64-x-x-x row">
+          <div class="col-sm-12">
+            <div class="m-16-x">
+              <h3>Final Design</h3>
+              <p>For any design project, determining the proper visual language is crucial. I started the homepage redesign for Expii by looking at the existing style library and make conscious changes that better advance user experience, facilitate the company to more effectively convey intended messages to the users, while still maintaining the overall look and feel of the whole ecosystem.</p>
+            </div>
           </div>
         </div>
       </div>
 
-<!--       <div class="m-16-x-x-x row-16">
-        <div class="col-xs-12">
-          <div class="p-x-16 m-16-x">
-            <video autoplay loop style="max-width: 100%">
-              <source src="img/hero-data-chart-sm.mp4" type="video/mp4">
-            Your browser does not support HTML5 video.
-            </video>
-          </div>
-        </div>
-      </div> -->
-
-      <div class="m-16-x-x-x row">
-        <div class="col-xs-12 col-sm-6">
-          <div class="p-x-16 m-16-x">
-            <video autoplay loop style="max-width: 100%">
-              <source src="img/students-residents.mp4" type="video/mp4">
-            Your browser does not support HTML5 video.
-            </video>
-          </div>
-        </div>
-        <div class="col-xs-12 col-sm-6">
-          <div class="p-x-16 m-16-x">
-            <video autoplay loop style="max-width: 100%">
-              <source src="img/students-residents.mp4" type="video/mp4">
-            Your browser does not support HTML5 video.
-            </video>
-          </div>
-        </div>
-      </div>
-
-      <div class="m-16-x-x-x row">
-        <div class="col-xs-12">
-          <div class="p-x-16 m-16-x">
-            <video autoplay loop style="max-width: 100%">
-              <source src="img/tl-three-animation.mp4" type="video/mp4">
-            Your browser does not support HTML5 video.
-            </video>
-          </div>
-        </div>
+      <div class="body-container">
+    	  <div class="m-32-x row">
+    	    <div class="col-xs-12">
+    	      <div class="p-x-16 m-16-x">
+              <img src="img/oneliance.jpg">
+    	      </div>
+    	    </div>
+    	  </div>
       </div>
   	
     </div>
